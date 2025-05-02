351bc171d01b07b6e20c91636d6a2826ab0c650f
Evan Cendekia <evancendekia@mhcasiagroup.com>
2025-05-02 14:36:40 +0700

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by M. Evan Cendekia Suryandaru on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraTests/CameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTests.swift
+//  CameraTests
+//
+//  Created by M. Evan Cendekia Suryandaru on 02/05/25.
+//
+
+import Testing
+@testable import Camera
+
+struct CameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
